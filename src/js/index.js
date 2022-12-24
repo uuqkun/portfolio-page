9a1f1f0c437ba011d1dba977a36abca9ed5be901
Achmad Furqon Rachmadie <uuqkun@gmail.com>
2022-12-24 16:32:09 +0700

    responsive web pada bagian nav, hero section, dan about me

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,24 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyAL1NVFqCdwDkXKe_hlwOVWbq9eOskcYM9gk3bHTfFmpNeokb51UqBVwmnSKFwr0OwIg/exec'
 const form = document.forms['contact-form']
+const sendBtn = document.querySelector('.send-btn');
+const loadingBtn = document.querySelector('.loading-btn');
+const alertMessage = document.querySelector('.alert-message')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
+
+    sendBtn.classList.toggle('d-none')
+    loadingBtn.classList.toggle('d-none')
+
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => console.log('Success!', response))
+        .then(response => { 
+            sendBtn.classList.toggle('d-none')
+            loadingBtn.classList.toggle('d-none')
+            alertMessage.classList.toggle('d-none');
+
+            form.reset();
+
+            console.log('Success!', response) 
+        })
         .catch(error => console.error('Error!', error.message))
 })
